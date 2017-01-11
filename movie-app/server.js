d54a8a17a31f7aa6783b8fefe5137f58fad1842f
Augustė Nausėdaitė <auguste.nausedaite@gmail.com>
2017-01-11 16:35:42 +0100

    added gitignore and a simple backend

---
+++ movie-app/server.js
@@ -0,0 +1,74 @@
+// all the necessary modules
+const express = require('express')
+const app = express()
+// const mongoose = require('mongoose')
+const Sequelize = require('sequelize')
+// const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+
+// connect to db
+let db = new Sequelize( 'moviewatch', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+	server: 'localhost',
+	dialect: 'postgres'
+})
+
+let Towatch = db.define('towatch', {
+	text: Sequelize.STRING,
+	done: Sequelize.BOOLEAN
+})
+
+// use express
+app.use(express.static(__dirname + '/public'))              // set the static files location /public/img will be /img for users
+app.use(bodyParser.urlencoded({'extended':'true'}))       // parse application/x-www-form-urlencoded
+app.use(bodyParser.json())                                  // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })) // parse application/vnd.api+json as json
+app.use(methodOverride())
+
+app.get('/api/towatches', ( req, res ) => {
+	// display the to watch list
+	Towatch.findAll()
+	.then( data => {
+		res.json( data )
+	})
+})
+
+app.post('/api/towatches', ( req, res ) => {
+	// add a movie to watch
+	Towatch.create({
+		text: req.body.text,
+		done: false
+	})
+	Towatch.findAll()
+	.then( data => {
+		res.json( data )
+	})
+})
+
+app.delete('/api/towatches', ( req, res ) => {
+	Towatch.destroy({
+		where: { id: req.params.id }
+	})
+	Towatch.findAll()
+	.then( data => {
+		res.json( data )
+	})
+})
+
+// sync db
+db.sync({ force: true }).then( db => {
+	Towatch.create({
+		text: "Swiss Army Man",
+		done: false
+	})
+	Towatch.create({
+		text: "Home Alone",
+		done: false
+	})
+})
+
+// listening on port 8000
+app.listen(8000, () => {
+	console.log( 'I hear you!' )
+})
